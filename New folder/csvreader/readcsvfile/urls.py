697912383fb1f2bb8da370275177b19dd5f00c66
pawara101 <pawara101dassanayake@gmail.com>
2023-10-21 22:18:11 +0530

    Json Response obtaoned | 2023-10-21

--- New folder/csvreader/readcsvfile/urls.py
+++ New folder/csvreader/readcsvfile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CSVItemView
 
 urlpatterns = [
-    path('csvdata/', CSVItemView.as_view(), name='csvdata'),
+    path('csvdata/', CSVItemView, name='csvdata'),
 ]
--- New folder/csvreader/readcsvfile/views.py
+++ New folder/csvreader/readcsvfile/views.py
@@ -1,34 +1,17 @@
 import csv
 import json
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Item  # Import your Item model
-from .serializers import ItemSerializer  # Import your ItemSerializer
+from django.http import JsonResponse
 
-class CSVItemView(APIView):
-    def get(self, request, *args, **kwargs):
-        try:
-            csv_file_path = 'C:/Users/USER/OneDrive/Python3/django/Octopus Task/Code/data2.csv'
+def CSVItemView(request,i=10):
+    # Specify the path to your CSV file
+    csv_file_path = 'C:/Users/USER/OneDrive/Python3/django/Octopus Task/Code/data1.csv'
 
-            with open(csv_file_path, 'r') as file:
-                reader = csv.DictReader(file)
-                data = []
+    data = []
 
-                # Assuming your CSV columns match your Item model fields
-                for row in reader:
-                    data.append(row)
-
-            # Serialize the data using your ItemSerializer
-            serializer = ItemSerializer(data=data, many=True)
-
-            # Check if the data is valid and return it as JSON
-            if serializer.is_valid():
-                return Response(serializer.data)
-            else:
-                # Log or print serializer.errors to identify the specific issue
-                print(serializer.errors)
-                return Response({'message': 'Data serialization error.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        except FileNotFoundError:
-            return Response({'message': 'CSV file not found.'}, status=status.HTTP_404_NOT_FOUND)
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    
+    # Send JSON data as a response
+    return JsonResponse(data, safe=False)
